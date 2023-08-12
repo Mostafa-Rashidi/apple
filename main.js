a50c84fbedf547099b3a8cd1d3e73d4e53501bf1
mostafa.rashidi <mostafa.rashidinasab@gmail.com>
2023-08-12 11:18:31 +0330

    part-1

---
+++ main.js
@@ -0,0 +1,263 @@
+// li-2 - store
+let store = document.querySelector("a[data-bs-toggle=store]");
+let mn2 = store.nextElementSibling ;
+
+store.addEventListener("mouseenter" , function(){
+    store.nextElementSibling.classList.add("d-block");
+    store.nextElementSibling.classList.remove("d-none");
+});
+mn2.addEventListener("mouseenter" , function(){
+    store.nextElementSibling.classList.add("d-block");
+    store.nextElementSibling.classList.remove("d-none");
+});
+store.addEventListener("mouseleave" , function(){
+    store.nextElementSibling.classList.add("d-none");
+    store.nextElementSibling.classList.remove("d-block");
+
+});
+mn2.addEventListener("mouseleave" , function(){
+    store.nextElementSibling.classList.add("d-none");
+    store.nextElementSibling.classList.remove("d-block");
+
+});
+
+
+
+// li-3 - mac
+let mac = document.querySelector("a[data-bs-toggle=mac]");
+let mn3 = mac.nextElementSibling;
+mac.addEventListener("mouseenter" , function(){
+    mac.nextElementSibling.classList.add("d-block");
+    mac.nextElementSibling.classList.remove("d-none");
+});
+mn3.addEventListener("mouseenter" , function(){
+    mac.nextElementSibling.classList.add("d-block");
+    mac.nextElementSibling.classList.remove("d-none");
+});
+mn3.addEventListener("mouseleave" , function(){
+    mac.nextElementSibling.classList.add("d-none");
+    mac.nextElementSibling.classList.remove("d-block");
+});
+mac.addEventListener("mouseleave" , function(){
+    mac.nextElementSibling.classList.add("d-none");
+    mac.nextElementSibling.classList.remove("d-block");
+});
+
+
+
+// li-4 - ipad
+let ipad = document.querySelector("a[data-bs-toggle=ipad]");
+let mn4 = ipad.nextElementSibling;
+ipad.addEventListener("mouseenter" , function(){
+    ipad.nextElementSibling.classList.add("d-block");
+    ipad.nextElementSibling.classList.remove("d-none");
+});
+mn4.addEventListener("mouseenter" , function(){
+    ipad.nextElementSibling.classList.add("d-block");
+    ipad.nextElementSibling.classList.remove("d-none");
+});
+ipad.addEventListener("mouseleave" , function(){
+    ipad.nextElementSibling.classList.add("d-none");
+    ipad.nextElementSibling.classList.remove("d-block");
+});
+mn4.addEventListener("mouseleave" , function(){
+    ipad.nextElementSibling.classList.add("d-none");
+    ipad.nextElementSibling.classList.remove("d-block");
+});
+
+
+
+// li-5 - iphone
+let iphone = document.querySelector("a[data-bs-toggle=iphone]");
+let mn5 = iphone.nextElementSibling;
+iphone.addEventListener("mouseenter" , function(){
+    iphone.nextElementSibling.classList.add("d-block");
+    iphone.nextElementSibling.classList.remove("d-none");
+});
+mn5.addEventListener("mouseenter" , function(){
+    iphone.nextElementSibling.classList.add("d-block");
+    iphone.nextElementSibling.classList.remove("d-none");
+});
+iphone.addEventListener("mouseleave" , function(){
+    iphone.nextElementSibling.classList.add("d-none");
+    iphone.nextElementSibling.classList.remove("d-block");
+});
+mn5.addEventListener("mouseleave" , function(){
+    iphone.nextElementSibling.classList.add("d-none");
+    iphone.nextElementSibling.classList.remove("d-block");
+});
+
+
+
+// li-6 - watch
+let watch = document.querySelector("a[data-bs-toggle=watch]");
+let mn6 = watch.nextElementSibling ;
+watch.addEventListener("mouseenter" , function(){
+    watch.nextElementSibling.classList.add("d-block");
+    watch.nextElementSibling.classList.remove("d-none");
+});
+mn6.addEventListener("mouseenter" , function(){
+    watch.nextElementSibling.classList.add("d-block");
+    watch.nextElementSibling.classList.remove("d-none");
+});
+watch.addEventListener("mouseleave" , function(){
+    watch.nextElementSibling.classList.add("d-none");
+    watch.nextElementSibling.classList.remove("d-block");
+});
+mn6.addEventListener("mouseleave" , function(){
+    watch.nextElementSibling.classList.add("d-none");
+    watch.nextElementSibling.classList.remove("d-block");
+});
+
+
+
+// li-8 - airpods
+let airpods = document.querySelector("a[data-bs-toggle=airpods]");
+let mn8 = airpods.nextElementSibling ;
+airpods.addEventListener("mouseenter" , function(){
+    airpods.nextElementSibling.classList.add("d-block");
+    airpods.nextElementSibling.classList.remove("d-none");
+});
+mn8.addEventListener("mouseenter" , function(){
+    airpods.nextElementSibling.classList.add("d-block");
+    airpods.nextElementSibling.classList.remove("d-none");
+});
+airpods.addEventListener("mouseleave" , function(){
+    airpods.nextElementSibling.classList.add("d-none");
+    airpods.nextElementSibling.classList.remove("d-block");
+});
+mn8.addEventListener("mouseleave" , function(){
+    airpods.nextElementSibling.classList.add("d-none");
+    airpods.nextElementSibling.classList.remove("d-block");
+});
+
+
+
+// li-9 - tv&home
+let tvhome = document.querySelector("a[data-bs-toggle=tvhome]");
+let mn9 = tvhome.nextElementSibling ;
+tvhome.addEventListener("mouseenter" , function(){
+    tvhome.nextElementSibling.classList.add("d-block");
+    tvhome.nextElementSibling.classList.remove("d-none");
+});
+mn9.addEventListener("mouseenter" , function(){
+    tvhome.nextElementSibling.classList.add("d-block");
+    tvhome.nextElementSibling.classList.remove("d-none");
+});
+tvhome.addEventListener("mouseleave" , function(){
+    tvhome.nextElementSibling.classList.add("d-none");
+    tvhome.nextElementSibling.classList.remove("d-block");
+});
+mn9.addEventListener("mouseleave" , function(){
+    tvhome.nextElementSibling.classList.add("d-none");
+    tvhome.nextElementSibling.classList.remove("d-block");
+});
+
+
+
+// li-10 - intertainment
+let intertainment = document.querySelector("a[data-bs-toggle=intertainment]");
+let mn10 = intertainment.nextElementSibling ;
+intertainment.addEventListener("mouseenter" , function(){
+    intertainment.nextElementSibling.classList.add("d-block");
+    intertainment.nextElementSibling.classList.remove("d-none");
+});
+mn10.addEventListener("mouseenter" , function(){
+    intertainment.nextElementSibling.classList.add("d-block");
+    intertainment.nextElementSibling.classList.remove("d-none");
+});
+intertainment.addEventListener("mouseleave" , function(){
+    intertainment.nextElementSibling.classList.add("d-none");
+    intertainment.nextElementSibling.classList.remove("d-block");
+});
+mn10.addEventListener("mouseleave" , function(){
+    intertainment.nextElementSibling.classList.add("d-none");
+    intertainment.nextElementSibling.classList.remove("d-block");
+});
+
+
+
+// li-11 - accessibility
+let accessibility = document.querySelector("a[data-bs-toggle=accessibility]");
+let mn11 = accessibility.nextElementSibling ;
+accessibility.addEventListener("mouseenter" , function(){
+    accessibility.nextElementSibling.classList.add("d-block");
+    accessibility.nextElementSibling.classList.remove("d-none");
+});
+mn11.addEventListener("mouseenter" , function(){
+    accessibility.nextElementSibling.classList.add("d-block");
+    accessibility.nextElementSibling.classList.remove("d-none");
+});
+accessibility.addEventListener("mouseleave" , function(){
+    accessibility.nextElementSibling.classList.add("d-none");
+    accessibility.nextElementSibling.classList.remove("d-block");
+});
+mn11.addEventListener("mouseleave" , function(){
+    accessibility.nextElementSibling.classList.add("d-none");
+    accessibility.nextElementSibling.classList.remove("d-block");
+});
+
+// li-12 - support
+let support = document.querySelector("a[data-bs-toggle=support]");
+let mn12 = support.nextElementSibling;
+support.addEventListener("mouseenter" , function(){
+    support.nextElementSibling.classList.add("d-block");
+    support.nextElementSibling.classList.remove("d-none");
+});
+mn12.addEventListener("mouseenter" , function(){
+    support.nextElementSibling.classList.add("d-block");
+    support.nextElementSibling.classList.remove("d-none");
+});
+support.addEventListener("mouseleave" , function(){
+        support.nextElementSibling.classList.add("d-none");
+        support.nextElementSibling.classList.remove("d-block");        
+});
+mn12.addEventListener("mouseleave" , function(){
+    support.nextElementSibling.classList.add("d-none");
+    support.nextElementSibling.classList.remove("d-block");        
+});
+    
+
+
+// li-13 - search
+let search = document.querySelector("a[data-bs-toggle=search]");
+let mn13 = search.nextElementSibling;
+console.log(mn13);
+search.addEventListener("click" , function(){
+    search.nextElementSibling.classList.add("d-block");
+    search.nextElementSibling.classList.remove("d-none");
+});
+mn13.addEventListener("mouseenter" , function(){
+    search.nextElementSibling.classList.add("d-block");
+    search.nextElementSibling.classList.remove("d-none");
+});
+search.addEventListener("mouseleave" , function(){
+    search.nextElementSibling.classList.add("d-none");
+    search.nextElementSibling.classList.remove("d-block");
+});
+mn13.addEventListener("mouseleave" , function(){
+    search.nextElementSibling.classList.add("d-none");
+    search.nextElementSibling.classList.remove("d-block");
+});
+
+
+
+// li-14 - basket
+let basket = document.querySelector("a[data-bs-toggle=basket]");
+let mn14 = basket.nextElementSibling;
+basket.addEventListener("click" , function(){
+    basket.nextElementSibling.classList.add("d-block");
+    basket.nextElementSibling.classList.remove("d-none");
+});
+mn14.addEventListener("mouseenter" , function(){
+    basket.nextElementSibling.classList.add("d-block");
+    basket.nextElementSibling.classList.remove("d-none");
+});
+basket.addEventListener("mouseleave" , function(){
+    basket.nextElementSibling.classList.add("d-none");
+    basket.nextElementSibling.classList.remove("d-block");
+});
+mn14.addEventListener("mouseleave" , function(){
+    basket.nextElementSibling.classList.add("d-none");
+    basket.nextElementSibling.classList.remove("d-block");
+});
